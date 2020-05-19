ddb80e90398794e332f6613a10477e1ff0c079f1
Amit Agrawal <amitagrawal10500@gmail.com>
2020-05-19 20:05:23 +0530

    added take call button

--- src/BusinessPage.js
+++ src/BusinessPage.js
@@ -4,12 +4,35 @@ class BusinessPage extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            value: "unavailable",
+            showCallPickupButton: false
+        }
+    }
 
+    handleChange = (e) => {
+        this.setState({ value: e.target.value });
+        console.log("You have selected " + e.target.value);
     }
 
     render() {
         return (
-            <h1>Welcome to Business Page</h1>
+            <div>
+                <h1>Welcome to Business Page</h1>
+                <section>
+                    <select value={this.state.value} onChange={this.handleChange} >
+                        <option value="available">Available</option>
+                        <option value="unavailable">UnAvailable</option>
+                    </select>
+                </section>
+                {
+                    this.state.value === "available" &&
+                    <section>
+                        <br />
+                        <button>PickUpCall</button>
+                    </section>
+                }
+            </div>
         )
     }
 
